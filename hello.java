3ed9b3809e7c040e3eceacf303d6d5f8998a3f26
ricky <ricky@gmail.com>
2022-09-02 13:58:05 +0000

    add hello program

---
+++ hello.java
@@ -0,0 +1,9 @@
+class forLoopDemo {
+    public static void main(String args[])
+    {
+        // Writing a for loop
+        // to print Hello World 5 times
+        for (int i = 1; i <= 5; i++)
+            System.out.println("Hello devops engineer");
+    }
+}
